02c79b7281e26fe3abd061d09f3e51aea580b671
Qedir-Mikayilzade <130463028+Qedir-Mikayilzade@users.noreply.github.com>
2023-11-14 14:53:06 +0400

    Add files via upload

--- Currency-converter/app.js
+++ Currency-converter/app.js
@@ -1,29 +1,33 @@
-let inputL = document.querySelector('.input-left');
-let inputR = document.querySelector('.input-right');
+let inputL = document.querySelector('#amountL');
+let inputR = document.querySelector('#amountR');
 let btnsL = document.querySelector('.btns-left');
 let btnsR = document.querySelector('.btns-right');
-// let btns = document.querySelector('.btns');
-
-// btns.addEventListener('click',(e)=>{
-//     const clickedBtn = e.target;
-//     btns.querySelectorAll('.btn').forEach(otherBtn=>{
-//         otherBtn.classList.remove('selected');
-//     })
-//     clickedBtn.classList.add('selected');
-// })
-
-btnsL.addEventListener('click',(e)=>{
-    const clickedBtn = e.target;
-    btnsL.querySelectorAll('.btn').forEach(otherBtn=>{
-        otherBtn.classList.remove('selected');
-    })
-    clickedBtn.classList.add('selected');
+let btnL = document.querySelectorAll('.btns-left .btn');
+let btnR = document.querySelectorAll('.btns-right .btn');
+const api = 'https://api.exchangerate.host/live?access_key=f6338752102ad791591c5ae4dd7b66b7'; 
+btnL.forEach(otherBtn=>{
+        otherBtn.addEventListener('click',(e)=>{
+            const clickedBtn = e.target;
+                btnsL.querySelectorAll('.btn').forEach(otherBtn=>{
+                    otherBtn.classList.remove('selected');
+                });
+                clickedBtn.classList.add('selected');
+                inputL.focus();
+            });
+        });
+        btnR.forEach(otherBtn=>{
+            otherBtn.addEventListener('click',(e)=>{
+                const clickedBtn = e.target;
+                btnsR.querySelectorAll('.btn').forEach(otherBtn=>{
+                    otherBtn.classList.remove('selected');
+                });
+                clickedBtn.classList.add('selected');
+                inputR.focus();
+    });
 });
 
-btnsR.addEventListener('click',(e)=>{
-    const clickedBtn = e.target;
-    btnsR.querySelectorAll('.btn').forEach(otherBtn=>{
-        otherBtn.classList.remove('selected');
-    })
-    clickedBtn.classList.add('selected');
-});
+fetch('http://api.exchangerate.host/convert?access_key=f6338752102ad791591c5ae4dd7b66b7?from=EUR&to=GBP&amount=100')
+.then(res=>res.json())
+.then(data=>{
+    console.log(data);
+})
